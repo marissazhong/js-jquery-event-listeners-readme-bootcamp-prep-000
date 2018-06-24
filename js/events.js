267200cb87e30ab09481744b8fce3f243879f361
Marissa Zhong <mwz2101@columbia.edu>
2018-06-24 18:59:25 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -10,7 +10,7 @@ function frameIt() {
 
 function pressIt() {
   $(document).on('keydown', function(key) {
-    if (key.which == 7) {
+    if (key.which == 71) {
       alert('G was pressed');
     }
   });
