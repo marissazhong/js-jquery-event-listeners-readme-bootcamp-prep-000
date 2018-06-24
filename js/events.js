21d7083dab451551d68df8b629cbea2a90513090
Marissa Zhong <mwz2101@columbia.edu>
2018-06-24 18:41:48 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,6 +1,10 @@
 //define functions here
 function getIt() {
-  $('p').on("click", function() {alert("Hey!")})
+  $('p').on("click", function(key) {
+    if () {
+      alert("Hey!");
+    }
+  })
 }
 
 function frameIt() {
