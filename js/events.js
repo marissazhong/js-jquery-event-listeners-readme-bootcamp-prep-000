15963f9bd40310c1f4c0f6e5da5577ac202f2fbf
Marissa Zhong <mwz2101@columbia.edu>
2018-06-24 19:01:12 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -18,7 +18,7 @@ function pressIt() {
 
 function submitIt() {
   $(document).on("submit", function() {
-    alert('your form is going to be submitted now.');
+    alert('Your form is going to be submitted now.');
     return;
   });
 }
